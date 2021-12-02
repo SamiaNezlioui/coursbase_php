fdbff9e1da7ae0ffabccdb12e969a8613ebf0beb
SamiaNezlioui <84906495+SamiaNezlioui@users.noreply.github.com>
2021-12-02 14:46:12 +0100

    Numbers

---
+++ numbers.php
@@ -0,0 +1,65 @@
+<?php
+echo 202;
+echo "<br>";
+//addition
+echo 30 + 20 ; 
+
+// multiplication
+echo "<br>";
+echo 6 * 2;
+
+//Division
+echo "<br>";
+echo 10 / 2;
+
+//la fonction pow() =>Exponential expression
+echo "<br>";
+echo pow(5,2);
+echo pow(2,2);
+// la fonction sqrt() =>Racine carrée(square root)
+echo "<br>";
+echo sqrt(12);
+
+echo "<br>";
+echo sqrt(9);
+
+echo "<br>";
+echo round(3.5);
+echo "<br>";
+echo round(3.2);
+// la fonction Floor() =>enléve ce qui il se trouve aprés la vergule pour Arrondir chiffre
+
+echo "<br>";
+echo floor(3.8);
+
+echo "<br>";
+echo ceil(19.128875);
+
+echo "<br>";
+echo 10 % 3;//=> le restede la division
+
+echo "<br>";
+$num = 7;
+$num ++;
+echo $num;
+
+echo "<br>";
+$num --;
+echo $num;
+
+echo "<br>";
+$num +=7;
+echo $num;
+
+echo "<br>";
+$num /= 7;
+echo $num;
+
+//function max() => renvoie la valeur la plus élevée
+echo "<br>";
+echo max(2 , 10);
+//function min() => renvoie la valeur la plus élevée
+echo "<br>";
+echo min(2 , 10);
+
+?>
