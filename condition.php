316fd6bd9d05b94014b04f7350005ac29f958174
SamiaNezlioui <84906495+SamiaNezlioui@users.noreply.github.com>
2021-12-10 13:52:56 +0100

    les condition

---
+++ condition.php
@@ -0,0 +1,36 @@
+
+<?php
+$username = "Hajer";
+$email = "r@gamail.com" ;
+
+if($username === "Hajer" || 
+$email === "hjr@gamail.com")
+{
+    echo"Welcome Admin";
+}
+ 
+if($email === "hjr@gamail.com"){
+    echo"<br>";
+    echo "Welcome Admin email";
+}
+?>
+
+<?php
+/*
+$number1 = 20;
+$number2 = 10;
+
+if ($number1 > $number2){
+    echo "le chiffre le plus gros";
+
+        if($number1 === 20){
+            echo"<br>";
+            echo ("the firsty number it vingt");
+        }
+}elseif($number1 < $number2){
+    echo "le chiffre le plus petit";
+}else{
+    echo "le chffre est équal petit";
+}
+*/
+?>
